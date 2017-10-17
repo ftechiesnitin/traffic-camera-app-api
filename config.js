ad8b0ae06fd476a89b9e062a2cbf96f2376ce5ae
Nitin Dolli <nitin.dolli@gmail.com>
2017-10-17 21:48:29 +0800

    api part initiated

---
+++ config.js
@@ -0,0 +1,5 @@
+var config = module.exports = {};
+
+// App configuration
+config.env = 'dev';
+config.port = '3000';
---
+++ server.js
@@ -0,0 +1,88 @@
+require('use-strict');
+const express = require('express');
+const app = express();
+const http = require('http');
+const bodyParser = require('body-parser');
+const compression = require('compression');
+// Helmet helps you secure your Express apps by setting various HTTP headers
+const helmet = require('helmet');
+// This package protects against HTTP Parameter Pollution attacks
+const hpp = require('hpp');
+
+// Get configuration
+const cfg = require('./config.js');
+
+app.use(helmet());
+// compress all requests response in GZIP
+app.use(compression());
+// handle cors
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
+  res.header('Access-Control-Allow-Credentials', false);
+  next();
+});
+app.use(bodyParser.json({ limit: '50mb', extended: true, type:'application/json' }));
+app.use(bodyParser.json({ type: 'application/*+json' }));
+app.use(hpp());
+
+app.get('/api', (req, res) => {
+  res.jsonp({hey: 'hello'});
+});
+
+/**
+ * Create HTTP server.
+ */
+var server = http.createServer(app);
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+server.listen(normalizePort(cfg.port));
+server.on('error', onError);
+server.on('listening', onListening);
+
+/**
+ * Normalize a port into a number, string, or false.
+ */
+
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) return val;
+  if (port >= 0) return port;
+
+  return false;
+}
+
+/**
+ * Event listener for HTTP server "error" event.
+ */
+
+function onError(error) {
+  if (error.syscall !== 'listen') throw error;
+
+  var bind = typeof port === 'string' ? 'Pipe ' + port : 'Port ' + port;
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+function onListening() {
+  var addr = server.address();
+  var bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port;
+  console.log('Listening on: ' + bind);
+}
